37ebd9988e68398eb897e80d9b1cf52ae419002a
Nick Welch <mackstann@gmail.com>
2020-01-14 13:10:57 -0800

    Implement POST /schedule_interview endpoint

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Code exercise: An interview scheduling tool
 
 # How to Build and Run
 
+I'm using Flask, which is a small framework to make serving HTTP cleaner/easier/nicer.
+
 This project uses Python 3, so be careful not to accidentally use Python 2 if you have it installed.
 
     virtualenv env # or maybe virtualenv-3 etc.
@@ -11,3 +13,25 @@ This project uses Python 3, so be careful not to accidentally use Python 2 if yo
     FLASK_ENV=development FLASK_APP=schedule.py flask run
 
 The UI will now be available at http://localhost:5000/
+
+## `POST /schedule_interview` curl examples
+
+Success -> HTTP 204:
+
+    curl -v -X POST http://localhost:5000/schedule_interview -d '{"id": 2}' -H "Content-Type: application/json"
+
+Try to POST JSON without the `id` field -> HTTP 400:
+
+    curl -v -X POST http://localhost:5000/schedule_interview -d '{"foo": 2}' -H "Content-Type: application/json"
+
+Try to schedule a wrongly-typed time slot ID -> HTTP 400:
+
+    curl -v -X POST -s http://localhost:5000/schedule_interview -d '{"id": "hello"}' -H "Content-Type: application/json"
+
+Try to schedule a nonexistent slot ID -> HTTP 409:
+
+    curl -v -X POST -s http://localhost:5000/schedule_interview -d '{"id": 1}' -H "Content-Type: application/json"
+
+Try to POST a non-JSON body -> HTTP 415:
+
+    curl -v -X POST -s http://localhost:5000/schedule_interview -d 'foo'
--- schedule.py
+++ schedule.py
@@ -13,3 +13,34 @@ def index():
 @app.route('/employer_schedules', methods=['GET'])
 def employer_schedules():
     return jsonify(data.employer_schedules)
+
+@app.route('/schedule_interview', methods=['POST'])
+def schedule_interview():
+    if not request.is_json:
+        # HTTP 415 Unsupported Media Type: ... indicates that the server refuses to accept the request because the
+        # payload format is in an unsupported format.
+        return error_response(415, 'Only JSON request body is supported')
+
+    try:
+        slot_id = request.json['id']
+    except KeyError:
+        return error_response(400, '`id` field required')
+
+    if not isinstance(slot_id, int):
+        return error_response(400, '`id` must be an integer')
+
+    if slot_id not in (slot['id'] for slot in data.employer_schedules):
+        # HTTP 409 Conflict: The request could not be completed due to a conflict with the current state of the target
+        # resource. This code is used in situations where the user might be able to resolve the conflict and resubmit
+        # the request.
+        #
+        # 400 would also make some sense, but maybe the id submitted is not /wrong/ per se -- maybe it's just bad timing
+        # and that slot did exist a minute ago, and was, say, deleted.
+        return error_response(409, 'Unknown time slot')
+
+    # Here we'd normally do something to actually book the time slot.
+
+    return '', 204 # Success: HTTP 204 No Content
+
+def error_response(code, message):
+    return jsonify({'error': message}), code, {'Content-Type': 'application/json'}
