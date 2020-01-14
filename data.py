3a8c050c30d0d7309fb44699292fd9ab895e6fb4
Nick Welch <mackstann@gmail.com>
2020-01-14 12:38:18 -0800

    Implement GET /employer_schedules endpoint

---
+++ data.py
@@ -0,0 +1,5 @@
+employer_schedules = [
+    {"id": 2, "start_time": "2019-02-28T16:00:00.000Z"},
+    {"id": 6, "start_time": "2019-02-28T21:00:00.000Z"},
+    {"id": 9, "start_time": "2019-02-28T14:00:00.000Z"},
+]
--- schedule.py
+++ schedule.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, abort, url_for
 from flask.json import jsonify
 
+from . import data
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     # serve the template etc.
     pass
+
+@app.route('/employer_schedules', methods=['GET'])
+def employer_schedules():
+    return jsonify(data.employer_schedules)
